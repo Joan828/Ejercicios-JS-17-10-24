229053d7d4759698a2d9c0a4a04e1f78fae6814e
unknown <joan_mart_her@hotmail.com>
2024-10-17 16:55:25 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// 1. Formulario de contacto - Local Storage
+
+// Crear un formulario de contacto con los siguientes campos:
+// Nombre
+// Correo
+// Mensaje
+// Crea una función que cuando envíes el formulario muestre por consola los datos de contacto rellenados en un objeto de JS
+
+const nombre = document.getElementById("nombre")
+const correo = document.getElementById("correo")
+const mensaje = document.getElementById("mensaje")
+
+const nombreValor = nombre.value
+const correoValor = correo.value
+const mensajeValor = mensaje.value
+
+let datosUsuario = {
+  Nombre: nombreValor,
+  Correo: correoValor,
+  Mensaje: mensajeValor
+}
+
+function mostrarDatos(e){
+  e.preventDefault()
+  console.log(datosUsuario)
+
+}
+
+const btn = document.getElementById("btn")
+btn.addEventListener("click",mostrarDatos)
+
+// Guardar en Local Storage el objeto consoleado antes (sólo guarda un usuario)
+localStorage.setItem('user', JSON.stringify(datosUsuario))
+
+// Muestra el usuario que has guardado en el DOM (en el HTML)
+
+const usuario = localStorage.getItem('user')
+document.body.innerHTML = "Nombre: " + JSON.parse(usuario).Nombre +"<br>Correo: " + JSON.parse(usuario).Correo + "<br>Mensaje: " + JSON.parse(usuario).Mensaje
+
+
